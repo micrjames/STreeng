c93d96ce678ee4e2e7e0bbc0b800db315ad127c8
Michael James <micrjames921@gmail.com>
2024-11-05 23:57:10 -0700

    setup project and basic test

---
+++ tests/index.test.ts
@@ -0,0 +1,13 @@
+import { STreeng } from "../STreeng";
+
+describe("A tree", () => {
+   let streeng: STreeng;
+   beforeAll(() => {
+	  streeng = new STreeng();
+   });
+   describe("With no nodes", () => {
+	  test.todo("Should exist.");
+	  test.todo("Should entail an empty string.");
+	  test.todo("Should not be printed.");
+   });
+});
